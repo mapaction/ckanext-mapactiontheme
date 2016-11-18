008119e44f764a47e0c534dba0ed1e906fa55e5d
tomd <tomd+git@aptivate.org>
2016-11-18 12:57:45 +0000

    Remove unused static media

--- ckanext/mapactiontheme/plugin.py
+++ ckanext/mapactiontheme/plugin.py
@@ -324,7 +324,6 @@ class MapactionthemePlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'mapactiontheme')
 
     #IAuthFunctions
     def get_auth_functions(self):
