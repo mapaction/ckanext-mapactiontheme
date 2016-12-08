9ec1703655b0fe79f7972455d4af7c830cd4d414
Martin Burchell <martinb@aptivate.org>
2016-12-08 16:51:29 +0000

    Reinstate fanstatic resource
    It looks like this was removed accidentally, along with some JavaScript
    that was no longer needed.
    
    We still have JavaScript to show and hide the CKAN login form

--- ckanext/mapactiontheme/plugin.py
+++ ckanext/mapactiontheme/plugin.py
@@ -340,6 +340,7 @@ class MapactionthemePlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'mapactiontheme')
 
     #IAuthFunctions
     def get_auth_functions(self):
