ee7abc28d75012fce2973c47853008c5eccf1240
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2019-04-13 15:48:08 +0100

    attempt to update to PGv9.6

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -5,7 +5,25 @@ echo "This is travis-build.bash..."
 
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
-sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java:amd64=1.2.2-1
+#sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java:amd64=1.2.2-1
+
+# We need to install PostgreSQL v9.6. On Ubuntu 18.04 PostgreSQL v10 is the default version.
+# Therefore we have adapted these instructions from this page
+# https://wiki.postgresql.org/wiki/Apt
+#sudo apt install curl ca-certificates
+#curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+#sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main 9.6" > /etc/apt/sources.list.d/pgdg.list'
+#sudo apt update
+#sudo apt install postgresql-9.6 postgresql-server-dev-9.6
+
+# Install Tomcat:
+sudo apt-get install solr-tomcat
+ 
+# To restart tomcat:
+sudo service tomcat8 restart
+
+# Use this command to install the required packages:
+sudo apt-get install python-dev  python-pip python-virtualenv git-core openjdk-8-jdk redis-serv
 
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
