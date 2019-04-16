d67b00038c3bebdec8c6d31d1b709c6410a8039c
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2019-04-16 21:34:31 +0100

    run tomcat instead of jetty

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -2,5 +2,5 @@
 
 echo "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
-sudo service jetty restart
+sudo service tomcat7 restart
 nosetests --nologcapture --with-pylons=subdir/test.ini --with-coverage --cover-package=ckanext.mapactiontheme --cover-inclusive --cover-erase --cover-tests
